33c026f0f0721a5953fefa36e22ca6e082e60485
Lucas Yago <lucasyago63@gmail.com>
2022-07-19 17:17:27 -0300

    adiciona tema e styles.xml

--- app/src/main/java/com/lucasyago/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/lucasyago/gastoviagem/MainActivity.kt
@@ -6,13 +6,9 @@ import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var editText: EditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        editText = findViewById(R.id.etDistance)
-        editText.setTextAppearance(R.style.BlueEditText)
-    }
 
+    }
 }
