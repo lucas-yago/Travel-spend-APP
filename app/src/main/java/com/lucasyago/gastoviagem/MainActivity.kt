b7d099869107bf4392a9c6c1c0460ced6454cde7
Lucas Yago <lucasyago63@gmail.com>
2022-07-19 10:55:46 -0300

    altera thema do layout

--- app/src/main/java/com/lucasyago/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/lucasyago/gastoviagem/MainActivity.kt
@@ -2,10 +2,17 @@ package com.lucasyago.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        editText = findViewById(R.id.etDistance)
+        editText.setTextAppearance(R.style.BlueEditText)
     }
+
 }
