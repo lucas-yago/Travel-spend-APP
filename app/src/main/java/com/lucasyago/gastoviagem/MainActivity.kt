3ca692227954f427d531c16be017c7108f3c25fa
Lucas Yago <lucasyago63@gmail.com>
2022-07-20 01:26:34 -0300

    Implementa funçao calculate e evento do botao de calculo

--- app/src/main/java/com/lucasyago/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/lucasyago/gastoviagem/MainActivity.kt
@@ -1,19 +1,38 @@
 package com.lucasyago.gastoviagem
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.EditText
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.lucasyago.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnButtonCalculate.setOnClickListener(this)
+
     }
-    private fun calculate(){
-        //TODO Nescessario implementar funçao calculate
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btnButtonCalculate) {
+            calculate()
+        }
     }
+
+    @SuppressLint("SetTextI18n")
+    private fun calculate() {
+
+        val distance = binding.etDistance.text.toString().toFloat()
+        val price = binding.etPrice.text.toString().toFloat()
+        val autonomy = binding.etAutonomy.text.toString().toFloat()
+
+        val totalValue = (distance * price) / autonomy
+        binding.tvTotalValue.text = "R$ %.2f".format(totalValue)
+
+    }
+
 }
